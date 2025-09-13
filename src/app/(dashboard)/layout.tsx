2eb0a5a2ad1c567f1afb62bfbf8de9a88d354f50
varunvarshney00 <varunvarshney6882@gmail.com>
2025-09-13 10:27:10 +0530

    dashboard-navbar

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,4 +1,5 @@
 import { SidebarProvider } from "@/components/ui/sidebar";
+import { DashboardNavbar } from "@/modules/dashboard/ui/components/dashboard-navbar";
 import { DashboardSidebar } from "@/modules/dashboard/ui/components/dashboard-sidebar";
 import React from "react";
 
@@ -9,8 +10,11 @@ interface Props {
 const Layout = ({ children }: Props) => {
   return (
     <SidebarProvider>
-        <DashboardSidebar />
-      <main className="flex flex-col h-screen w-screen bg-muted">{children}</main>
+      <DashboardSidebar />
+      <main className="flex flex-col h-screen w-screen bg-muted">
+        <DashboardNavbar />
+        {children}
+      </main>
     </SidebarProvider>
   );
 };
